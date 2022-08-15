89d6a2725a9548dc879814d1b7824ae461557616
AdrianPrzychodzien <adrian.przychodzien@gmail.com>
2022-08-15 17:52:26 +0200

    menu controller

---
+++ app/javascript/controllers/menu_controller.js
@@ -0,0 +1,62 @@
+import { Controller } from '@hotwired/stimulus';
+
+export default class extends Controller {
+    static targets = ['hamburger', 'mobile_menu', 'menu_list'];
+
+    toggle(e) {
+        const mobileMenuClassList = this.mobile_menuTarget.classList;
+
+        if (mobileMenuClassList.contains('hidden')) {
+            mobileMenuClassList.remove('hidden');
+            const menuLinksMobile = this.mobile_menuTarget.getElementsByTagName('a');
+            this.handleActiveLink(menuLinksMobile, true);
+        } else {
+            mobileMenuClassList.add('hidden');
+        }
+    }
+
+    handleLinkStyle(menuLink, isMobile) {
+        menuLink.classList.add('font-bold');
+
+        if (isMobile) menuLink.classList.add('text-red-700');
+    }
+
+    handleActiveLink(menuLinks, isMobile = false) {
+        const breadcrumbs = document.getElementById('breadcrumbs');
+        const pathname = window.location.pathname;
+        const is_teacher_dashboard = window.location.pathname.includes('teacher');
+
+        Array.from(menuLinks).forEach(menuLink => {
+            if (is_teacher_dashboard) {
+                const href_last_word = menuLink.href.split('/').pop();
+                if (pathname.includes(href_last_word)) {
+                    menuLink.classList.add('font-bold', 'border-blue-600', 'hover:text-blue-600');
+                } else {
+                    menuLink.classList.add('border-transparent');
+                }
+
+                return;
+            }
+
+            if (!breadcrumbs) {
+                const href_last_word = menuLink.href.split('/').pop();
+                if (pathname.includes(href_last_word)) {
+                    this.handleLinkStyle(menuLink, isMobile);
+                }
+
+                return;
+            }
+
+            Array.from(breadcrumbs.children).forEach(breadcrumb => {
+                if (breadcrumb.href === menuLink.href || breadcrumbs.baseURI === menuLink.href) {
+                    this.handleLinkStyle(menuLink, isMobile);
+                }
+            });
+        });
+    }
+
+    connect() {
+        const menuLinks = this.menu_listTarget.getElementsByTagName('a');
+        this.handleActiveLink(menuLinks);
+    }
+}
