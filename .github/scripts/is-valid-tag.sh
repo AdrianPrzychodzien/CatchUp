da71f28a5d71357955c977acaa2e4620ceccd016
Adrian Przychodzien <adrian@fuseignited.com>
2024-01-24 13:56:04 +0100

    fix bash

--- .github/scripts/is-valid-tag.sh
+++ .github/scripts/is-valid-tag.sh
@@ -17,5 +17,7 @@ Then, in your workflow, you can use it like this:
     if ! .github/scripts/is-valid-tag.sh "${GITHUB_REF}"; then
       echo "Invalid tag: ${GITHUB_REF}"
       exit 1
+    else
+      echo "Valid tag: ${GITHUB_REF}"
     fi
 ```
